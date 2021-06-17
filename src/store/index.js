4d20ceb185174e78dd5976602c5cc4f2c3cf96f6
hritikchokker <hchokker7@gmail.com>
2021-06-17 23:16:43 +0530

    state management update

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,27 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    formData: {
+      name: "",
+      occupation: "",
+      organization: "",
+    },
+  },
+  mutations: {
+    profileUpdate(state, payload) {
+      state.formData = {
+        name: payload.name || "",
+        occupation: payload.occupation || "",
+        organization: payload.organization || "",
+      };
+    },
+    profileClear(state) {
+      state.formData = {
+        name: "",
+        occupation: "",
+        organization: "",
+      };
+    },
+  },
 });
